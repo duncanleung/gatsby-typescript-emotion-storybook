ccd2bbbfd7b434dea70f87a8e55d18daba0acaec
Waseem Ahmed <codewaseem@gmail.com>
2020-04-11 13:53:08 +0530

    add styles to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { css } from "@emotion/core";
 import { ReactComponent as Logo } from "~/assets/images/logo.svg";
+
+// #region styles
+const getHeaderStyles = () => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+// #endregion
 
 const Header = () => {
   return (
-    <header>
-      <Logo />
+    <header css={getHeaderStyles()}>
+      <Logo
+        css={css`
+          display: block;
+          width: 128px;
+          height: 32px;
+        `}
+      />
+      hi
     </header>
   );
 };
